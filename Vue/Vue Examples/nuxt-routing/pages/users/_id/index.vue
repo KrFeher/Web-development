be4e8eacc71d03c773317165c8b719dd49703f6d
KrFeher <34376587+KrFeher@users.noreply.github.com>
2018-07-20 08:09:00 +0100

    Pushed again nuxt-route project and removed subproject git structure

---
+++ Vue/Vue Examples/nuxt-routing/pages/users/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>A single user with ID : {{$route.params.id}} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    validate(data) {
+        return !isNaN(data.params.id);
+    }
+}
+</script>
+
+
+
---
+++ Vue/Vue Examples/nuxt-routing/pages/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Users Page</h1>
+        <h2>Select a user:</h2>
+        <input type="text" v-model="userId"> <button @click="goToPage">Go</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                userId: ''
+            }
+        },
+        methods: {
+            goToPage() {
+                this.$router.push("/users/" + this.userId)
+            }
+        }
+    }
+</script>
