b6fef514f1049ff08af31c63644eec2ef3d3e1cc
KrFeher <34376587+KrFeher@users.noreply.github.com>
2018-07-27 08:56:58 +0100

    Moved form to a component

---
+++ Vue/Vue Examples/Nuxt-blog/components/Admin/AdminPostForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form @submit.prevent="onSave">
+      <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+      <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+      <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+      <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+      <AppButton type="submit">Save</AppButton>
+      <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
+    </form>
+  </div>
+</template>
+
+<script>
+  import AppButton from '@/components/UI/AppButton.vue';
+  import AppControlInput from '@/components/UI/AppControlInput.vue';
+
+  export default {
+    data() {
+      return {
+        editedPost: {
+          author: '',
+          title: '',
+          thumbnailLink: '',
+          content: ''
+        }
+      }
+    },
+    components: {
+      AppButton,
+      AppControlInput
+    },
+    methods: {
+      onCancel() {
+        this.$router.push('/admin')
+      },
+      onSave() {
+        console.log(this.editedPost)
+      }
+    }
+  }
+</script>
---
+++ Vue/Vue Examples/Nuxt-blog/pages/admin/_postId/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm />
+    </section>
+  </div>
+</template>
+
+<script>
+  import AdminPostForm from '@/components/Admin/AdminPostForm.vue';
+
+  export default {
+    components: {
+      AdminPostForm
+    },
+    data() {
+      return {
+        loadedPost: {
+          author: 'Krisztian Feher',
+          title: 'Hello Blog',
+          thumbnailLink: 'https://picsum.photos/500/300/?random',
+          content: 'This is the first blog post'
+        }
+      }
+    },
+  }
+</script>
